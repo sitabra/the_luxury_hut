ccf9eeddcb51af5b44d739d08da21d11935e4808
sitabra <sitabra.das@cbnits.com>
2022-09-08 12:42:14 +0530

    bottom navivation bar screens added

---
+++ lib/constants/dimens.dart
@@ -0,0 +1,49 @@
+class AppSizes {
+  static const double appBarHeight = 60.0;
+  static const double mainHeaderHeight = 80.0;
+  static const double textFieldHeight = 48.0;
+  static const double buttonHeight = 44.0;
+
+  static const double largeButtonHeight = 50.0;
+
+  static const double smallButtonHeight = 20.0;
+
+
+  static const double cardCornerRadius = 6.0;
+
+  static const double dimen30 = 30.0;
+  static const double dimen127 = 127.0;
+  static const double dimen125 = 125.0;
+  static const double dimen55 = 55.0;
+  static const double dimen60 = 60.0;
+  static const double dimen24 = 24.0;
+  static const double dimen16 = 16.0;
+  static const double dimen12 = 12.0;
+  static const double dimen13 = 13.0;
+  static const double dimen8 = 8.0;
+  static const double dimen9 = 9.0;
+  static const double dimen4 = 4.0;
+
+  static const double dimen5 = 5.0;
+
+  static const double dimen2 = 2.0;
+
+
+  static const double dimen3 = 3.0;
+
+
+  static const double elevation0 = 0.0;
+  static const double elevation1 = 1.0;
+
+  static const double headline1 = 34.0;
+  static const double headline2 = 28.0;
+  static const double headline3 = 26.0;
+  static const double headline4 = 22.0;
+  static const double headline5 = 18.0;
+  static const double headline6 = 16.0;
+  static const double bodyText1 = 15.0;
+  static const double bodyText2 = 13.0;
+  static const double caption = 11.0;
+  static const double button = 16.0;
+  static const double smallButton = 12.0;
+}
---
+++ lib/constants/palatte.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+class AppColors {
+  static const white = Color(0xFFFFFFFF);
+  static const scaffold = Color(0xFFFFFFFF);
+  static const header = Color(0xFF6C6B6B);
+  static const black = Color(0xFF000000);
+  static const darkGrey = Color(0xFF323232);
+  static const lightGrey = Color(0xFFC2C1C1);
+  static const grey = Color(0xFF8E8D8D);
+  static const textFieldBg = Color(0xFFF3F3F3);
+  static const transparent = Color(0xFFFFF);
+  static const shimmer = Color(0xFFF1F3F5);
+  static const orange = Color(0xF7FC7732);
+  static const amber = Color(0xF7E8A73E);
+  static const green = Color(0xFF23842D);
+  static const lightGreen = Color(0xFF1F803C);
+  static const facebook = Color(0xFF3b5998);
+  static const twitter = Color(0xFF00ACEE);
+  static const instagram = Color(0xFF833AB4);
+  static const youtube = Color(0xFFc4302b);
+}
